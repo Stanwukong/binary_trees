809ec04dac883f2ca80cb391eb27ac8ae0d8d445
Oluwafemi S.E. Ajanaku <111810384+Stanwukong@users.noreply.github.com>
2024-02-09 13:38:14 +0000

    Switching to vim

--- binary_trees.h
+++ binary_trees.h
@@ -22,6 +22,7 @@ typedef struct binary_tree_s
 	struct binary_tree_s *right;
 } binary_tree_t;
 
+/* Printing helper */ 
 void binary_tree_print(const binary_tree_t *);
 
 #endif /* _BINARY_TREES_H_ */
