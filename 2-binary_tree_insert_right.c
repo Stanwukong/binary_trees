2eaf1815397538c7028efc2dccd045acbe9272ad
Stanwukong <stanleyajanaku@gmail.com>
2024-02-10 02:31:16 -0800

    Code optimization

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,25 +12,18 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node, *temp_node;
 
-	new_node = malloc(sizeof(binary_tree_t));
+	new_node = binary_tree_node(parent, value);
+
 	if (new_node == NULL || parent == NULL)
 		return (NULL);
-
-	new_node->parent = parent;
-	new_node->n = value;
-	new_node->left = NULL;
 
 	if (parent->right != NULL)
 	{
 		new_node->right = parent->right;
-		temp_node = parent->right;
-		parent->right = new_node;
-		new_node->right = temp_node;
+		parent->right->parent = new_node;
 	}
-	else
-	{
-		parent->right = new_node;
-	}
+
+	parent->right = new_node;
 
 	return (new_node);
 }
